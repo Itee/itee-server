614ed1d773224c657da137f669a42e575e41157b
IG\tvalcke <tvalcke@geomap-imagis.com>
2017-12-20 11:03:21 +0100

    Add default resources

---
+++ resources/configs/config.undefined.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tristan on 17/09/2017.
+ */
+
+const TClientAppParameters = {
+    error: (function UndefinedIteeClientConfiguration() {
+        console.error("The TClientAppParameters are not defined, or the file not existing on the actual server. Please accept our appologies !");
+    })()
+}
