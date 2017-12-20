96844c1ffc152f9c194ff6e186176bf26c32049f
IG\tvalcke <tvalcke@geomap-imagis.com>
2017-12-20 15:41:47 +0100

    ClientConfig: fix variable type from const to let

--- resources/configs/config.undefined.js
+++ resources/configs/config.undefined.js
@@ -2,7 +2,7 @@
  * Created by Tristan on 17/09/2017.
  */
 
-const TClientAppParameters = {
+let TClientAppParameters = {
     error: (function UndefinedIteeClientConfiguration() {
         console.error("The TClientAppParameters are not defined, or the file not existing on the actual server. Please accept our appologies !");
     })()
