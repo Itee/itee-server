1277855d0a444b039f95c6c6521bccbe9495718c
IG\tvalcke <tvalcke@geomap-imagis.com>
2018-01-08 23:14:20 +0100

    Move mergeDeep and isObject methods from index.pug to itee-app

--- assets/itee-app.js
+++ assets/itee-app.js
@@ -9,6 +9,40 @@ import * as Client from '../node_modules/itee-client/sources/itee-client'
 
 (() => {
 
+    function isObject ( item ) {
+        return (item && typeof item === 'object' && !Array.isArray( item ));
+    }
+
+    function mergeDeep ( target, source ) {
+
+        var output = Object.assign( {}, target )
+
+        if ( isObject( target ) && isObject( source ) ) {
+
+            var keys = Object.keys( source )
+
+            for ( var i = 0, n = keys.length ; i < n ; ++i ) {
+
+                var key = keys[ i ]
+
+                if ( isObject( source[ key ] ) ) {
+
+                    if ( !(key in target) ) {
+                        Object.assign( output, { [key]: source[ key ] } )
+                    } else {
+                        output[ key ] = mergeDeep( target[ key ], source[ key ] )
+                    }
+
+                } else {
+
+                    Object.assign( output, { [key]: source[ key ] } )
+
+                }
+            }
+        }
+        return output;
+    }
+
     // Check if webgl is available
     const message = Client.WebGLDetector.getWebGLErrorMessage()
     if ( message.innerText ) {
