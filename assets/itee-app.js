c8292556b4fad5a9c087f3fcc24639948fa54e08
IG\tvalcke <tvalcke@geomap-imagis.com>
2018-02-20 16:36:44 +0100

    Update index.pug and add directly itee-client build during app creation Reactoring

--- assets/itee-app.js
+++ assets/itee-app.js
@@ -5,65 +5,50 @@
  * @license MIT
  */
 
-import * as Client from 'itee-client'
-//import * as Client from '../node_modules/itee-client/sources/itee-client'
-
+//import * as Itee from 'itee-client'
+//import * as Itee from '../node_modules/itee-client/sources/itee-client'
+import * as Itee from './itee-client.es'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 (() => {
 
-    function isObject ( item ) {
-        return (item && typeof item === 'object' && !Array.isArray( item ));
-    }
-
-    function mergeDeep ( target, source ) {
-
-        var output = Object.assign( {}, target )
-
-        if ( isObject( target ) && isObject( source ) ) {
-
-            var keys = Object.keys( source )
-
-            for ( var i = 0, n = keys.length ; i < n ; ++i ) {
-
-                var key = keys[ i ]
-
-                if ( isObject( source[ key ] ) ) {
-
-                    if ( !(key in target) ) {
-                        Object.assign( output, { [key]: source[ key ] } )
-                    } else {
-                        output[ key ] = mergeDeep( target[ key ], source[ key ] )
-                    }
-
-                } else {
-
-                    Object.assign( output, { [key]: source[ key ] } )
-
-                }
-            }
-        }
-        return output;
-    }
-
     // Check if webgl is available
-    const message = Client.Detector.getWebGLErrorMessage()
+    const message = Itee.Detector.getWebGLErrorMessage()
     if ( message.innerText ) {
         alert( message.innerText )
         return
     }
 
     // Create App
-    // Retrieve parameters
-    const appContainer  = document.getElementById( 'mainContainer' )
-    const appParameters = mergeDeep( window.TConfigParameters || {}, window.TUrlParameters || {} )
-    const appOnReady    = function () {}
+    const appView = {
+//        type:  Itee.TButton,
+//        props: {
+//            key:   'TButton_0',
+//            label: 'MyAwesomeButton'
+//        }
+    }
 
-    const clientApp = new Client.TApplication( appContainer, appParameters, appOnReady )
+    const appModel = {}
 
-    window.addEventListener( 'resize', function () {
+    const appContainer = document.querySelector( 'itee-application-root' )
 
-        console.log( 'RESIZEEEEEEEEEEEEEE' )
+    const app          = React.createElement( Itee.TBasicApplication, appModel, appView )
 
-    } )
+    ReactDOM.render( app, appContainer )
+
+    //    // Create App
+    //    // Retrieve parameters
+    //    const appContainer  = document.getElementById( 'mainContainer' )
+    //    const appParameters = Itee.extend( window.TConfigParameters || {}, window.TUrlParameters || {} )
+    //    const appOnReady    = function () {}
+    //
+    //    const clientApp = new Client.TApplication( appContainer, appParameters, appOnReady )
+    //
+    //    window.addEventListener( 'resize', function () {
+    //
+    //        console.log( 'RESIZEEEEEEEEEEEEEE' )
+    //
+    //    } )
 
 })()
