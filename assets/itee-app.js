9a145fca92b46d999112fa2dee73f9438fe7675c
IG\tvalcke <tvalcke@geomap-imagis.com>
2018-02-22 12:13:01 +0100

    IteeApp: Update code to latest IteeClient version

--- assets/itee-app.js
+++ assets/itee-app.js
@@ -6,10 +6,7 @@
  */
 
 //import * as Itee from 'itee-client'
-//import * as Itee from '../node_modules/itee-client/sources/itee-client'
 import * as Itee from './itee-client.es'
-import React from 'react'
-import ReactDOM from 'react-dom'
 
 (() => {
 
@@ -21,34 +18,7 @@ import ReactDOM from 'react-dom'
     }
 
     // Create App
-    const appView = {
-//        type:  Itee.TButton,
-//        props: {
-//            key:   'TButton_0',
-//            label: 'MyAwesomeButton'
-//        }
-    }
-
-    const appModel = {}
-
-    const appContainer = document.querySelector( 'itee-application-root' )
-
-    const app          = React.createElement( Itee.TBasicApplication, appModel, appView )
-
-    ReactDOM.render( app, appContainer )
-
-    //    // Create App
-    //    // Retrieve parameters
-    //    const appContainer  = document.getElementById( 'mainContainer' )
-    //    const appParameters = Itee.extend( window.TConfigParameters || {}, window.TUrlParameters || {} )
-    //    const appOnReady    = function () {}
-    //
-    //    const clientApp = new Client.TApplication( appContainer, appParameters, appOnReady )
-    //
-    //    window.addEventListener( 'resize', function () {
-    //
-    //        console.log( 'RESIZEEEEEEEEEEEEEE' )
-    //
-    //    } )
+    const appParameters = Itee.extend( window.TConfigParameters || {}, window.TUrlParameters || {} )
+    const application = Itee.TApplicationFactory.createApplication( appParameters )
 
 })()
