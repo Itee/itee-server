a9bc5a2e6f8b21c5e916ad371cc279d4bbe52cfa
IG\tvalcke <tvalcke@geomap-imagis.com>
2019-08-02 22:38:55 +0200

    build(rollupconfig): update rollupconfig to latest itee standard

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -2,131 +2,126 @@
  * @author [Tristan Valcke]{@link https://github.com/Itee}
  * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  *
- * @module config/rollupConfiguration
- *
+ * @module Config-Rollup
  * @description The file manage the rollup configuration for build the library using differents arguments. It allow to build with two type of environment (dev and prod), and differents output format.
  * Use npm run help to display all available build options.
  *
- * @requires {@link module: [path]{@link https://nodejs.org/api/path.html}}
- * @requires {@link module: [rollup-plugin-buble]{@link https://github.com/rollup/rollup-plugin-buble}}
- * @requires {@link module: [rollup-plugin-node-resolve]{@link https://github.com/rollup/rollup-plugin-node-resolve}}
+ * @requires {@link module: [rollup-plugin-node-builtins]{@link https://github.com/calvinmetcalf/rollup-plugin-node-builtins}}
  * @requires {@link module: [rollup-plugin-commonjs]{@link https://github.com/rollup/rollup-plugin-commonjs}}
- * @requires {@link module: [rollup-plugin-replace]{@link https://github.com/rollup/rollup-plugin-replace}}
- * @requires {@link module: [rollup-plugin-uglify-es]{@link https://github.com/TrySound/rollup-plugin-uglify}}
- *
+ * @requires {@link module: [rollup-plugin-json]{@link https://github.com/rollup/rollup-plugin-json}}
+ * @requires {@link module: [path]{@link https://nodejs.org/api/path.html}}
+ * @requires {@link module: [rollup-plugin-re]{@link https://github.com/jetiny/rollup-plugin-re}}
+ * @requires {@link module: [rollup-plugin-node-resolve]{@link https://github.com/rollup/rollup-plugin-node-resolve}}
+ * @requires {@link module: [rollup-plugin-terser]{@link https://github.com/TrySound/rollup-plugin-terser}}
  */
 
-/* eslint-env node */
-
-const path        = require( 'path' )
-const babel       = require( 'rollup-plugin-babel' )
-const nodeResolve = require( 'rollup-plugin-node-resolve' )
-const commonJs    = require( 'rollup-plugin-commonjs' )
-const replace     = require( 'rollup-plugin-replace' )
-const uglify      = require( 'rollup-plugin-uglify-es' )
-
-function glsl () {
-
-    return {
-        transform ( code, id ) {
-            if ( !/\.glsl$/.test( id ) ) {
-                return
-            }
-
-            var transformedCode = 'export default ' + JSON.stringify(
-                code
-                    .replace( /[ \t]*\/\/.*\n/g, '' )
-                    .replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' )
-                    .replace( /\n{2,}/g, '\n' )
-            ) + ';'
-            return {
-                code: transformedCode,
-                map:  { mappings: '' }
-            }
-        }
-    }
-
-}
+const builtins = require( 'rollup-plugin-node-builtins' )
+const commonjs = require( 'rollup-plugin-commonjs' )
+const json = require( 'rollup-plugin-json' )
+const path = require( 'path' )
+const replace = require( 'rollup-plugin-re' )
+const resolve = require( 'rollup-plugin-node-resolve' )
+const terser = require( 'rollup-plugin-terser' ).terser
 
 /**
  * Will create an appropriate configuration object for rollup, related to the given arguments.
  *
- * @param {string} format - The desired module format output. Available values are: 'es', 'cjs', 'amd', 'iife' and 'umd'
- * @param {boolean} onProduction - The building environment. True for production, developement otherwise.
- * @param {boolean} wantSourceMap - Set to true if sourcemap must be include in the output.
- * @returns {object} The rollup configuration
+ * @generator
+ * @param options
+ * @return {Array.<json>} An array of rollup configuration
  */
-function CreateRollupConfiguration ( format, onProduction, wantSourceMap ) {
+function CreateRollupConfigs ( options ) {
+    'use strict'
 
-    const _format        = format || 'umd'
-    const _onProduction  = onProduction || false
-    const _wantSourceMap = wantSourceMap || false
+    const name = options.name
+    const input = options.input
+    const output = options.output
+    const formats = options.format.split( ',' )
+    const envs = options.env.split( ',' )
+    const sourcemap = options.sourcemap
+    const treeshake = options.treeshake
+    const fileName = path.basename( input, '.js' )
 
-    const fileName       = 'itee-app'
-    const fileExtension  = (_onProduction) ? '.min.js' : '.js'
-    const inputFilePath  = path.join( __dirname, '..', `assets/${fileName}.js` )
-    const outputFilePath = path.join( __dirname, '..', `resources/application/${fileName}.${_format}${fileExtension}` )
+    const configs = []
 
-    return {
-        inputOptions:  {
+    for ( let formatIndex = 0, numberOfFormats = formats.length ; formatIndex < numberOfFormats ; ++formatIndex ) {
 
-            // core options
-            input:    inputFilePath,
-            external: [],
-            plugins:  [
-                glsl(),
-                commonJs( {
-                    include: 'node_modules/**'
-                } ),
-                babel( require( './babel.conf' )( _onProduction ) ),
-                replace( {
-                    'process.env.NODE_ENV': JSON.stringify( (_onProduction) ? 'production' : 'development' )
-                } ),
-                nodeResolve(),
-                onProduction && uglify()
-            ],
+        for ( let envIndex = 0, numberOfEnvs = envs.length ; envIndex < numberOfEnvs ; envIndex++ ) {
 
-            // advanced options
-            onwarn: function onWarn ( { loc, frame, message } ) {
-                if ( loc ) {
-                    process.stderr.write( `/!\\ WARNING: ${loc.file} (${loc.line}:${loc.column}) ${frame} ${message}\n` )
-                } else {
-                    process.stderr.write( `/!\\ WARNING: ${message}\n` )
+            const env = envs[ envIndex ]
+            const prod = ( env.includes( 'prod' ) )
+            const format = formats[ formatIndex ]
+            const outputPath = ( prod ) ? path.join( output, `${fileName}.${format}.min.js` ) : path.join( output, `${fileName}.${format}.js` )
+
+            configs.push( {
+                input:    input,
+                external: ( [ 'esm', 'cjs' ].includes( format ) ) ? [
+                    'fs',
+                    'path',
+                ] : [],
+                plugins: [
+                    replace( {
+                        defines: {
+                            IS_REMOVE: prod
+                        }
+                    } ),
+                    commonjs( {
+                        include: 'node_modules/**'
+                    } ),
+                    resolve( {
+                        preferBuiltins: true
+                    } ),
+                    json( {} ),
+                    ( [ 'iife', 'umd' ].includes( format ) ) && builtins( {
+                        fs: true
+                    } ),
+                    prod && terser()
+                ],
+                onwarn: ( { loc, frame, message } ) => {
+
+                    // Ignore some errors
+                    if ( message.includes( 'Circular dependency' ) ) { return }
+                    if ( message.includes( 'plugin uglify is deprecated' ) ) { return }
+
+                    if ( loc ) {
+                        process.stderr.write( `/!\\ ${loc.file} (${loc.line}:${loc.column}) ${frame} ${message}\n` )
+                    } else {
+                        process.stderr.write( `/!\\ ${message}\n` )
+                    }
+
+                },
+                treeshake: treeshake,
+                output:    {
+                    // core options
+                    file:    outputPath,
+                    format:  format,
+                    name:    name,
+                    globals: {},
+
+                    // advanced options
+                    paths:     {},
+                    banner:    '',
+                    footer:    '',
+                    intro:     '',
+                    outro:     '',
+                    sourcemap: sourcemap,
+                    interop:   true,
+
+                    // danger zone
+                    exports: 'auto',
+                    amd:     {},
+                    indent:  '\t',
+                    strict:  true
                 }
-            },
-            cache:  undefined,
+            } )
 
-            // danger zone
-            acorn:         undefined,
-            context:       undefined,
-            moduleContext: {},
-            legacy:        undefined
-        },
-        outputOptions: {
-            // core options
-            file:    outputFilePath,
-            format:  format,
-            name:    'Itee',
-            globals: {},
-
-            // advanced options
-            paths:     {},
-            banner:    '',
-            footer:    '',
-            intro:     '',
-            outro:     '',
-            sourcemap: _wantSourceMap,
-            interop:   true,
-
-            // danger zone
-            exports: 'auto',
-            amd:     {},
-            indent:  '  ',
-            strict:  true
         }
 
     }
 
+    return configs
+
 }
 
-module.exports = CreateRollupConfiguration
+module.exports = CreateRollupConfigs
+
